f536390e90daf7d6f608f08414313dcbcfa14e1d
Oleksandr1301 <viss82@ukr.net>
2023-07-14 00:26:47 +0300

    add mongodb

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,3 +1,31 @@
+// Z35RYfdCWk0xVUHU
+// trKVmKKIgmGGj13j
+// require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+const DB_URI = process.env.DB_URI;
+console.log({ DB_URI });
+
+const client = new MongoClient(DB_URI);
+
+async function run() {
+	try {
+		await client.connect();
+
+		await client.db("admin").command({ ping: 1 });
+		console.info("Pinged your deployment. You successfully connected to MongoDB!");
+	}
+	catch (err) {
+		console.error(err);
+		process.exit(1);
+	} finally {
+    
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 const express = require('express')
 const { listContacts, getContactById, addContact, removeContact, updateContact } = require('../../models/contacts')
 const { contactValidator } = require('../../validator/validator')
