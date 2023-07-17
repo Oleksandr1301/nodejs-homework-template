4f4f081d9bb73b484079af5e1e7167cfa7ab11ac
Oleksandr1301 <viss82@ukr.net>
2023-07-17 20:25:59 +0300

    update

---
+++ services/contactsServices.js
@@ -0,0 +1,38 @@
+const Contact = require("../models/Contact");
+
+const getAllContacts = async () => await Contact.find();
+
+const getContactById = async (contactId) => {
+  const allContacts = await getAllContacts();
+  const contactById = allContacts.find((contact) => contact.id === contactId);
+
+  return contactById || null;
+};
+
+
+const createContact = async ({ name, email, phone, favorite }) =>  {
+  return await Contact.create({ name, email, phone, favorite });
+};
+
+const updateContact = async (contactId, fields) => {
+  return await Contact.findByIdAndUpdate(contactId, fields, {
+    new: true,
+    strict: "throw",
+    runValidators: true,
+  });
+};
+
+const updateStatusContact = async (contactId, favorite) => {
+  return await Contact.findByIdAndUpdate(contactId, { favorite });
+};
+
+const deleteContact = async (contactId) => await Contact.findByIdAndRemove(contactId);
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  updateStatusContact,
+  deleteContact,
+};
